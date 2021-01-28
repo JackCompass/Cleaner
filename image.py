8672ddbfe59d1726c0495c7b1549cfc00a9630e3
Jack <kumaranuj1303@gmail.com>
2021-01-28 14:14:56 +0530

    Basic layout of the project

---
+++ image.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+import os
+import sys
+import hashlib
+
+
+
+if __name__ == '__main__':
+	""" If this file is imported in another file than this part of the code will not execute. """
+	if sys.argv[1]:
+		pass
+	else:
+		print("Path of the image folder is missing.")
